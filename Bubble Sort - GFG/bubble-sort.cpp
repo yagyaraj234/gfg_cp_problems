71bafbbdc85b6110826522b9eacfc363b16a73d4
Yagyaraj Lodhi <101352591+yagyaraj234@users.noreply.github.com>
2023-03-19 00:31:01 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.cpp
+++ Bubble Sort - GFG/bubble-sort.cpp
@@ -25,13 +25,16 @@ class Solution
     void bubbleSort(int arr[], int n)
     {
         // Your code here 
+        if(n==0 || n==1){
+            return;
+        }
+        
         for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-i-1;j++){
-                if(arr[j]>arr[j+1]){
-                    swap(arr[j],arr[j+1]);
-                }
+            if(arr[i]>arr[i+1]){
+                swap(arr[i],arr[i+1]);
             }
         }
+            bubbleSort(arr,n-1);
     }
 };
 
