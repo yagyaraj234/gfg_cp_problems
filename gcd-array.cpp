284afebb7c4660e155328685a2edfd026919bdcf
Yagyaraj Lodhi <101352591+yagyaraj234@users.noreply.github.com>
2023-04-29 03:08:25 +0530

    GCD Array GFG problem Solution

---
+++ gcd-array.cpp
@@ -0,0 +1,14 @@
+  int p = 0, ans = N;
+      for(int i=31; i >= 0; i--)
+      {
+          int bit = (X>>i) & 1;
+          if(bit == 1) p = p | (1<<i);
+          else
+          {
+              int temp = p | (1<<i);
+              int cnt = 0;
+              for(auto ele: A)  if((ele&temp) == temp) cnt++;
+              ans = min(ans,N-cnt);
+          }
+      }
+      return (ans);
