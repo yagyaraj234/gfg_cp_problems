0e6747e412b289a741d6724d1645e08fe0fc3f4f
Yagyaraj Lodhi <101352591+yagyaraj234@users.noreply.github.com>
2023-07-26 14:17:21 +0530

    Added solution - LeetHub

---
+++ Common elements - GFG/common-elements.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:    
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+
+            vector<int> ans;
+            
+            int i=0,j=0,k=0;
+            
+            // int prev1,prev2,prev3;
+            // prev1=prev2=prev3=INT_MIN;
+            
+            while(i< n1 && j<n2 && k<n3){
+                
+                if( A[i]== B[j] && B[j]== C[k]){
+                    int el=A[i];
+                    ans.push_back(el);
+                    while(i<n1 && A[i] == el)
+                    i++;
+                    
+                    while(j<n2 && B[j] == el)
+                    j++;
+                    
+                    while(k<n3 && C[k] == el)
+                    k++;
+                }
+                
+                else if( A[i]<B[j]){
+                    i++;
+                }
+                else if( B[j]<C[k]){
+                    j++;
+                }
+                else k++;
+            }
+            
+            return ans;
+        }
+        
+
+};
+
+//{ Driver Code Starts.
+
+int main ()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; 
+        cin >> n1 >> n2 >> n3;
+        int A[n1];
+        int B[n2];
+        int C[n3];
+        
+        for (int i = 0; i < n1; i++) cin >> A[i];
+        for (int i = 0; i < n2; i++) cin >> B[i];
+        for (int i = 0; i < n3; i++) cin >> C[i];
+        
+        Solution ob;
+        
+        vector <int> res = ob.commonElements (A, B, C, n1, n2, n3);
+        if (res.size () == 0) 
+            cout << -1;
+        for (int i = 0; i < res.size (); i++) 
+            cout << res[i] << " "; 
+        cout << endl;
+    }
+}
+// } Driver Code Ends
