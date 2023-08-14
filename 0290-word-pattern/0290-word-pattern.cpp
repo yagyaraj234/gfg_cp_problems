1ff0a15bfa02a759de7557835a5d579432846b01
Yagyaraj Lodhi <101352591+yagyaraj234@users.noreply.github.com>
2023-08-14 20:56:01 +0530

    Time: 0 ms (100%), Space: 6.3 MB (60.74%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool wordPattern(string p, string s) {
 
-       
+    //    First push string s all string into a vector
         vector<string> str;
         string push="";
         for(int i=0;i<s.size();i++){
@@ -17,23 +17,33 @@ public:
         }
         str.push_back(push);
 
+        // Check if the size does not match then direct return false
         if(p.size()!= str.size()) return false;
 
         unordered_map<char,string> ump;
         set<string> st;
 
         for(int i=0;i<p.size();i++){
+
+            // check the value of pattern[i] present on map or not
             if(ump.find(p[i])!=ump.end()){
+                // if the value is present in map
+                // and compare the value of pattern[i] and str[i] if it is not similar return false
                 if(ump[p[i]]!=str[i]){
                     return false;
                 }
             }
-            else {
-                if(st.count(str[i])>0){
+            // if the value of pattern of [i] does not present in the map
+            // check the count of str[i] in  the set if it is greater then 0 return 0;
+            else if(st.count(str[i])>0){
                     return false;
                 }
-                ump[p[i]]=str[i];
-                st.insert(str[i]);
+        // if all the above case does not match  then insert  string into the set 
+            else{
+                
+                ump[p[i]]=str[i]; 
+                
+                st.insert(str[i]);  // inserting string into set
             }
         }
 
