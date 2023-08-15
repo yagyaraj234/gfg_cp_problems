91eefc5d46684a5e40e7d3e7c7a8ec0b02f44a45
Yagyaraj Lodhi <101352591+yagyaraj234@users.noreply.github.com>
2023-08-15 12:35:07 +0530

    Time: 44 ms (81.99%), Space: 32.9 MB (37.96%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>> ans;
+        vector<vector<int>> ans; //Aggregate Answer
+        vector<int> ans1; //Elements of nums1 absent in nums2
+        vector<int> ans2; //Elements of nums1 absent in nums2
+        unordered_set<int>st1; //Unique elements of nums1
+        unordered_set<int> st2;//Unique elements of nums2
 
-        unordered_map<int,int> ump;
-        unordered_map<int,int> ump2;
-
-        for( int i : nums1){
-            ump[i]++;
+        for(int i = 0; i<nums1.size();i++){
+            st1.insert(nums1[i]);
         }
 
-        for(int i: nums2 ){
-            ump2[i]++;
+        for(int i = 0; i<nums2.size();i++){
+            st2.insert(nums2[i]);
         }
-        set<int> mini;
-        set<int> mini2;
-        for(int i=0;i<nums1.size();i++){
-            if(ump2.find(nums1[i])== ump2.end()){
-                mini.insert(nums1[i]);
-            }
-        }
-        for(int i=0;i<nums2.size();i++){
-            if(ump.find(nums2[i])== ump.end()){
-                mini2.insert(nums2[i]);
+
+        for(auto i:st1){ //Checking nums1 elements in nums2
+            if(st2.find(i)==st2.end()){
+                ans1.push_back(i); //Adding missing elements in ans1 vector
             }
         }
 
-        vector<int> s1(mini.begin(),mini.end());
-        vector<int> s2(mini2.begin(),mini2.end());
-        ans.push_back(s1);
-        ans.push_back(s2);
-     return ans;   
+        for(auto i:st2){ //Checking nums2 elements in nums1
+            if(st1.find(i)==st1.end()){
+                ans2.push_back(i); //Adding missing elements in ans2 vector
+            }
+        }
+       ans.push_back(ans1);
+       ans.push_back(ans2);
+       return ans; 
     }
 };
